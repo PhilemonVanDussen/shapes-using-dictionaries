65e124d591e12bd7e35193ec415dbb92122b4484
PhilemonVanDussen <vandussenph709@northwested.org>
2025-03-04 13:52:58 -0500

    yes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import pygame
 import sys
 import config # Import the config module
+import random
+import shapes
 
 def init_game():
     pygame.init()
@@ -22,12 +24,52 @@ def handle_events():
 def main():
     screen = init_game()
     clock = pygame.time.Clock() # Initalize the clock here
+
+    shapes_list = []
+            
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.GREEN) # Use color from config
-        pygame.display.flip()
+        screen.fill(config.WHITE) # Use color from config
 
+        shape_type = random.randrange(3)
+
+        if shape_type == 0:
+            new_shape = {
+            'type': 'circle',
+            'color': (random.randrange(255), random.randrange(255), random.randrange(255)),
+            'position': (random.randrange(config.WINDOW_WIDTH), random.randrange(config.WINDOW_HEIGHT)),
+            'radius': 50
+            }
+        elif shape_type == 1:
+                new_shape = {
+                'type': 'rectangle',
+                'color': (random.randrange(255), random.randrange(255), random.randrange(255)),
+                'position': (random.randrange(config.WINDOW_WIDTH - 100 ), random.randrange(config.WINDOW_HEIGHT - 100)),
+                'width': 100,
+                'height': 100
+            }
+        elif shape_type == 2:
+                new_shape = {
+                'type': 'line',
+                'color': (random.randrange(255), random.randrange(255), random.randrange(255)),
+                'start_pos': (random.randrange(config.WINDOW_WIDTH), random.randrange(config.WINDOW_HEIGHT)),
+                'end_pos': (random.randrange(config.WINDOW_WIDTH), random.randrange(config.WINDOW_HEIGHT)),
+                'width': 10
+            }
+    
+    shapes_list.append(new_shape)
+
+    for shape in shapes_list:
+        if shape['type'] == 'circle':
+            shapes.draw_circle(screen, shape)
+        elif shape['type'] == 'rectangle':
+            shapes.draw_rect(screen, shape)
+        elif shape['type'] == 'line':
+            shapes.draw_line(screen, shape)
+
+
+        pygame.display.flip()
         # Limit the frame rate to the specified frames per second
         clock.tick(config.FPS) # Use the clock to control the frame rate
 
